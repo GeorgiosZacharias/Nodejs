23b80d8c46836cada454a8003b7ccb706c8a14d6
yunggeo <zachariasg7@hotmail.gr>
2026-01-30 00:38:26 +0200

    simply router

--- server.js
+++ server.js
@@ -8,10 +8,16 @@ const server = http.createServer((req, res)=>{
     //res.setHeader('Content-Type','text/html');
 
     //res.end('<h1>Hello, World!</h1>');
-
-    res.writeHead(200, {'Content-Type':'application/json'}); //write head with status code and content type
-    res.end(JSON.stringify({message: 'Server TEST'}));
-});
+    if(req.url === '/'){
+    res.writeHead(200, {'Content-Type':'text/html'}); //write head with status code and content type
+    res.end(('<h1>HomePage</h1>'));
+    }else if(req.url === '/about'){
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.end(('<h1>About Page</h1>'));
+    }else{
+    res.writeHead(404, {'Content-Type':'text/html'});
+    res.end(('<h1>404 Not Found</h1>'));
+    }});    
 
 server.listen(port, ()=>{
     console.log(`Server is listening on port ${port}`);
