a7dc93a0e18e5a6acdf16e9a73a0416e7fe075bd
yunggeo <zachariasg7@hotmail.gr>
2026-01-29 19:10:51 +0200

    different content types json example

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const server = http.createServer((req, res)=>{
     //res.setHeader('Content-Type','text/html');
 
     //res.end('<h1>Hello, World!</h1>');
+
+    res.writeHead(200, {'Content-Type':'application/json'}); //write head with status code and content type
+    res.end(JSON.stringify({message: 'Server'}));
 });
 
 server.listen(port, ()=>{
