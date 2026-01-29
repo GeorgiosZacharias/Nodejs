9d5547e241b4c3fb280d55f20541dceccd50b811
yunggeo <zachariasg7@hotmail.gr>
2026-01-29 18:58:20 +0200

    es modules

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-const {generateRandomNumber,celsiusToFahrenheit} = require('./utils');
 
-console.log(`Random Numbver : ${generateRandomNumber()}`);
-console.log(`Celsius to Fahrenheit: ${celsiusToFahrenheit(0)}`);
+//const {generateRandomNumber,celsiusToFahrenheit} = require('./utils');
+//console.log(`Random Numbver : ${generateRandomNumber()}`);
+//console.log(`Celsius to Fahrenheit: ${celsiusToFahrenheit(0)}`);
+
+import { getPosts } from "./postController.js"; 
+console.log(getPosts());
