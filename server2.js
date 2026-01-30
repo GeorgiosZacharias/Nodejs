c527a2f24665f3085ffc3fa76a3fefa1acc4bed0
yunggeo <zachariasg7@hotmail.gr>
2026-01-30 16:00:38 +0200

    basic api route

--- server2.js
+++ server2.js
@@ -1,14 +1,26 @@
-import {createServer} from 'http';
+
+import { createServer } from 'http';
 
 
-const port = process.env.PORT;
+const port = process.env.PORT ;
+
 const users = [
     {id:1, name:'Alice'},
     {id:2, name:'Bob'},
     {id:3, name:'Charlie'}
 ];
-const server = createServer ((req, res)=>{
-    
+const server = createServer ((req, res)=>{ // Create HTTP server
+    if(req.url === '/api/users' && req.method === 'GET'){ // Check for specific route and method
+        res.setHeader('Content-Type','application/json');
+        res.write(JSON.stringify(users));
+        res.end();
+    }else{
+        res.setHeader('Content-Type','application/json');
+        res.statusCode = 404; // Not Found status
+        res.write(JSON.stringify({message: 'Route not found'}));
+        res.end();
+
+    }
 });
 
 
