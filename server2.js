665c7f253395a1d2c3f7ac3df68c74282a51502d
yunggeo <zachariasg7@hotmail.gr>
2026-01-30 16:05:48 +0200

    specific user with out a framework.harder

--- server2.js
+++ server2.js
@@ -13,8 +13,22 @@ const server = createServer ((req, res)=>{ // Create HTTP server
     if(req.url === '/api/users' && req.method === 'GET'){ // Check for specific route and method
         res.setHeader('Content-Type','application/json');
         res.write(JSON.stringify(users));
-        res.end();
-    }else{
+        res.end();    
+    }else if(req.url.match(/^\/api\/users\/([0-9]+)/) && req.method === 'GET'){ //harder without express
+         const id = req.url.split('/')[3];
+         const user = users.find((user) => user.id === parseInt(id));
+         if (user){
+            res.setHeader('Content-Type','application/json');
+            res.write(JSON.stringify(user));
+            res.end();
+         }else{
+            res.setHeader('Content-Type','application/json');
+            res.statusCode = 404; // Not Found status
+            res.write(JSON.stringify({message: 'User not found'}));
+            res.end();
+         }
+    }
+    else{
         res.setHeader('Content-Type','application/json');
         res.statusCode = 404; // Not Found status
         res.write(JSON.stringify({message: 'Route not found'}));
