a638740eb91dde949260255e5f5154a42885bd17
yunggeo <zachariasg7@hotmail.gr>
2026-02-06 13:26:05 +0200

    sychronous readfile

--- fsDemo.js
+++ fsDemo.js
@@ -10,3 +10,12 @@ fs.readFile('./test.txt','utf-8',(err, data )=> {
         console.log('File content:', data);
     }
 });
+
+//readFileSync() -- synchronous version
+try{ // This will block the event loop until the file is read ,
+//if it's large file, it can cause performance issues
+    const data = fs.readFileSync('./test.txt','utf-8');
+    console.log('File content (sync):', data);
+}catch(err){
+    console.error('Error reading file (sync):', err);
+}
