72d962d1abac41f74bdee3ac8a4b456b9ede159a
yunggeo <zachariasg7@hotmail.gr>
2026-02-06 13:37:46 +0200

    dirname()

--- pathDemo.js
+++ pathDemo.js
@@ -4,3 +4,7 @@ const filePath = './dir1/dir2/test.txt'; // Example file path
 
 //basename()
 console.log(path.basename(filePath)); // last portion of a path
+
+
+//dirname()
+console.log(path.dirname(filePath)); // directory portion of a path
