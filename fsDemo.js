11e9f183822a6d1e3ea75ae8a5d80dcfec19800b
yunggeo <zachariasg7@hotmail.gr>
2026-02-06 13:29:17 +0200

    promise then readefile.

--- fsDemo.js
+++ fsDemo.js
@@ -1,21 +1,30 @@
-import fs from 'fs';
+// e
+import fs from 'fs/promises';
 
 
-//readFile() -- callbac version
+// //readFile() -- callbac version
 
-fs.readFile('./test.txt','utf-8',(err, data )=> {
-    if(err){
-        console.error('Error reading file:', err);
-    }else{
-        console.log('File content:', data);
-    }
-});
+// fs.readFile('./test.txt','utf-8',(err, data )=> {
+//     if(err){
+//         console.error('Error reading file:', err);
+//     }else{
+//         console.log('File content:', data);
+//     }
+// });
 
-//readFileSync() -- synchronous version
-try{ // This will block the event loop until the file is read ,
-//if it's large file, it can cause performance issues
-    const data = fs.readFileSync('./test.txt','utf-8');
-    console.log('File content (sync):', data);
-}catch(err){
-    console.error('Error reading file (sync):', err);
+// //readFileSync() -- synchronous version
+// try{ // This will block the event loop until the file is read ,
+// //if it's large file, it can cause performance issues
+//     const data = fs.readFileSync('./test.txt','utf-8');
+//     console.log('File content (sync):', data);
+// }catch(err){
+//     console.error('Error reading file (sync):', err);
+// }
+
+//readFile() -- promise.then() version
+fs.readFile('./test.txt','utf-8').then(data => {
+    console.log('File content (promise):', data);
+}).catch(err => {
+    console.error('Error reading file (promise):', err);
 }
+)
